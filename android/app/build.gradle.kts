0429ec03e1a7ee3a87851518d5b6134e044dcf12
Foundersofisoft <Gleb_Founder@proton.me>
2025-08-03 05:00:47 +0600

    Update build.gradle.kts

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -1,19 +1,25 @@
+plugins {
+    id("com.android.application")
+    id("org.jetbrains.kotlin.android")
+    id("dev.flutter.flutter-gradle-plugin")
+}
+
 android {
-    namespace "com.example.zhaiyana"
-    compileSdkVersion 34
-    ndkVersion "27.0.12077973"
+    namespace = "com.example.zhaiyana"
+    compileSdk = 34
+    ndkVersion = "27.0.12077973"
 
     defaultConfig {
-        applicationId "com.example.zhaiyana"
-        minSdkVersion 21
-        targetSdkVersion 34
-        versionCode flutterVersionCode.toInteger()
-        versionName flutterVersionName
+        applicationId = "com.example.zhaiyana"
+        minSdk = 21
+        targetSdk = 34
+        versionCode = flutterVersionCode.toInt()
+        versionName = flutterVersionName
     }
 
     buildTypes {
         release {
-            signingConfig signingConfigs.debug
+            signingConfig = signingConfigs.getByName("debug")
         }
     }
 }
